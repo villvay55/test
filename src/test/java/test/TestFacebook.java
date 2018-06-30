62e835227c13846f0e3b0c068015301016560531
labaen <vijay.villvay@gmail.com>
2018-06-30 10:00:33 +0530

    commit

---
+++ src/test/java/test/TestFacebook.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+ 
+public class TestFacebook {
+ 
+@Test
+public void TestFireFox(){
+ 
+WebDriver driver=new FirefoxDriver();
+ 
+driver.manage().window().maximize();
+ 
+driver.get("http://www.facebook.com");
+ 
+driver.quit();
+ 
+}
+ 
+}
